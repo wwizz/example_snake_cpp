e435770bdd8369cc2aae75b7d90d6d58c60f89c7
Wies Hubbers <wies.hubbers@lighting.com>
2018-04-20 09:32:26 +0200

    added basic game loop, yet getchar blocks still

---
+++ SnakeGame.cpp
@@ -0,0 +1,5 @@
+//
+// Created by A Hubbers on 20/04/2018.
+//
+
+#include "SnakeGame.h"
---
+++ SnakeGame.h
@@ -0,0 +1,12 @@
+//
+// Created by A Hubbers on 20/04/2018.
+//
+
+#ifndef EXAMPLE_SNAKE_CPP_SNAKEGAME_H
+#define EXAMPLE_SNAKE_CPP_SNAKEGAME_H
+
+class SnakeGame {
+
+};
+
+#endif //EXAMPLE_SNAKE_CPP_SNAKEGAME_H
